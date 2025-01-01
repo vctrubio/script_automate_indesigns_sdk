c93da4e2b704a934609ff9132d17099dc1212ff1
vctrubio <vctrubio@gmail.com>
2025-01-01 20:38:49 +0100

    fire n img dir beast-mode

--- src/js-create-dir.js
+++ src/js-create-dir.js
@@ -41,6 +41,7 @@ function createDir(dirName) {
         }
 
         var skip = 0;
+        console.log('\x1b[34m%s\x1b[0m', 'createDir :PITSTOP:')
         for (let i = 0; i < jsonObject.properties.length; i++) {
             const property = jsonObject.properties[i];
             const property_url = property['Property-Url']
@@ -50,7 +51,6 @@ function createDir(dirName) {
                 fs.mkdirSync(`../${propertyDir}`);
             } else {
                 if (skip > 0) continue;
-
                 console.log(`\x1b[33m%s\x1b[0m`, `WARNING: ${propertyDir} already exists`);
                 const response = prompt('Directory already exists. Do you want to overwrite it? (y/n/q/c): ');
 
@@ -102,12 +102,16 @@ function listProperties(jsonObject) {
 }
 
 
-
+/* do and destroy -property-img-a/b/c/d.jpeg
+for later reading of indesign document
+*/
 async function firePropertyImageDir(jsonObject) {
     const propertyUrls = listProperties(jsonObject);
 
+    console.log('\x1b[34m%s\x1b[0m', 'firePropertiesImages :PITSTOP:')
     try {
 
+        let skipFlag = 0
         for (const propertyUrl of propertyUrls) {
             const propertyDir = `${dirPropertiesName}${propertyUrl}`;
             const photoDir = `../${propertyDir}/images`;
@@ -115,24 +119,60 @@ async function firePropertyImageDir(jsonObject) {
             if (!fs.existsSync(`${photoDir}`)) {
                 fs.mkdirSync(`${photoDir}`);
                 console.log(`\x1b[32m%s\x1b[0m`, `SUCCESS: ${photoDir} created`);
-            }
-            else {
+            } else {
                 console.log(`\x1b[33m%s\x1b[0m`, `WARNING: ${photoDir} already exists`);
-                // const response = prompt`${photoDir} Directory already exists. Do you want to overwrite it? (y/n/q/c): `;
+                if (skipFlag < 1) {
+                    const response = prompt('Do you want to overwrite it? (y/n/q/c): ');
+                    if (response.toLowerCase() === 'y') {
+                        console.log(`Directory ${photoDir}: overwritten successfully!`);
+                    } else if (response.toLowerCase() === 'c') {
+                        skipFlag++
+                    }
+                    else if (response.toLowerCase() === 'n') {
+                        console.log('\x1b[30m%s\x1b[0m', `Directory ${photoDir}: not overwritten.`);
+                        continue;
+                    }
+                    else if (response.toLowerCase() === 'q') {
+                        console.log('\x1b[30m%s\x1b[0m', `Safe exit from IMAGES PROCESS: ${photoDir} not overwritten.`);
+                        return;
+                    }
+
+                }
             }
 
             const getImages = jsonObject.properties.filter(property => property['Property-Url'] === propertyUrl)[0]['Cover-Img'];
             let letter = 'a'.charCodeAt(0);
+            let skip = 0;
+
+            console.log('\x1b[34m%s\x1b[0m', ' createImgaes :PITSTOP:')
             for (const img of getImages) {
+                if (skip > 0) continue
                 const letterTransform = String.fromCharCode(letter);
                 const path = `${photoDir}/${letterTransform}.jpeg`;
-                const savedPath = await downloadImage(img, path)
+                if (fs.existsSync(path)) {
+                    const response = prompt(`File ${path} already exists. Do you want to overwrite it? (y/n/q/c): `);
+
+                    if (response.toLowerCase() === 'y') {
+                        console.log(`File ${path}: overwritten successfully!`);
+                    } else if (response.toLowerCase() === 'q') {
+                        console.log('\x1b[30m%s\x1b[0m', `Safe exit. File ${path} not overwritten.`);
+                        return;
+                    } else if (response.toLowerCase() === 'n') {
+                        console.log('\x1b[30m%s\x1b[0m', `File ${path}: without changes.`);
+                        continue;
+                    } else if (response.toLowerCase() === 'c') {
+                        console.log(`Continuing...`);
+                        skip++
+                    }
+                }
+
+                if (skip < 1)
+                    await downloadImage(img, path);
                 letter++;
             }
         }
-    }
-    catch {
-        console.error('Error in firePropertyImageDir function ')
+    } catch {
+        console.error('Error in firePropertyImageDir function');
     }
     console.log(`\x1b[32m%s\x1b[0m`, `firedPropertyImages`);
 }
@@ -158,7 +198,7 @@ function downloadImage(url, outputPath) {
 
 function main() {
     jsonObject = getJsonType(jsonFilePath);
-    createDir(dirPropertiesName);
+    // createDir(dirPropertiesName);
     firePropertyImageDir(jsonObject);
 }
 
